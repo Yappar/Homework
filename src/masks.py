3e4825852c7bf07fab1875a74dfd09242f547351
linar <linar-88@mail.ru>
2025-04-18 14:07:15 +0500

    final version 9.2

--- src/masks.py
+++ src/masks.py
@@ -1,17 +1,12 @@
 # card_number = input("введите номер карты: 7158300734726758")
-mask_account = input("введите номер счета: 35383033474447895560 :")
+# mask_account = input("введите номер счета: 35383033474447895560 :")
 
 
-# def get_mask_card_number(card_number: str) -> str:
-#     """Функция маскировки номера банковской карты"""
-#     return f"{card_number[-16:-12]} {card_number[-12:-10]}** **** {card_number[-4:]}"
-#
-#
-# print(get_mask_card_number(card_number))
+def get_mask_card_number(card_number: str) -> str:
+    """Функция маскировки номера банковской карты"""
+    return f"{card_number[-16:-12]} {card_number[-12:-10]}** **** {card_number[-4:]}"
+
 
 def get_mask_account(mask_account: str) -> str:
     """Функция маскировки номера банковского счета"""
     return f"**{mask_account[-4:]}"
-
-
-print(get_mask_account(mask_account))
--- src/widget.py
+++ src/widget.py
@@ -1,19 +1,18 @@
-c_b_account = input("введите номер счета:")
+from src.masks import get_mask_account, get_mask_card_number
+
+c_b_account = input("введите номер карты или счета: ")
 
 
 def mask_account_card(c_b_account: str) -> str:
     """Функция, которая возвращает строку с замаскированным номером"""
-    len_list = 0
-    mask_name = ""
+    len_list = ""
     for symbol in c_b_account:
-        if symbol.isalpha() == True:
-            mask_name += symbol
         if symbol.isdigit() == True:
-            len_list += 1
-    if len_list == 16:
-        return f"{mask_name} {c_b_account[-16:-12]} {c_b_account[-12:-10]}** ****{c_b_account[-4:]}"
+            len_list += symbol
+    if len(len_list) == 16:
+        return f"{c_b_account[:-16]}{get_mask_card_number(len_list)}"
     else:
-        return f"{mask_name} **{c_b_account[-4:]}"
+        return f"{c_b_account[:-20]}{get_mask_account(len_list)}"
 
 
 print(mask_account_card(c_b_account))
@@ -22,7 +21,7 @@ data_input = input("Введите дату:")
 
 
 def get_date(data_input: str) -> str:
-    """Функция, которая возвращает строку с датой в формате (ДД.ММ.ГГГГ) """
+    """Функция, которая возвращает строку с датой в формате (ДД.ММ.ГГГГ)"""
     return f"{data_input[8:10]}.{data_input[5:7]}.{data_input[:4]}"
 
 
