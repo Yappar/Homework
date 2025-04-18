f8d6bd6a0823d4204aba79d6a0edc9b7ead832fa
linar <linar-88@mail.ru>
2025-04-18 15:16:32 +0500

    final ver 9.2

--- src/masks.py
+++ src/masks.py
@@ -1,7 +1,3 @@
-# card_number = input("введите номер карты: 7158300734726758")
-# mask_account = input("введите номер счета: 35383033474447895560 :")
-
-
 def get_mask_card_number(card_number: str) -> str:
     """Функция маскировки номера банковской карты"""
     return f"{card_number[-16:-12]} {card_number[-12:-10]}** **** {card_number[-4:]}"
@@ -10,3 +6,9 @@ def get_mask_card_number(card_number: str) -> str:
 def get_mask_account(mask_account: str) -> str:
     """Функция маскировки номера банковского счета"""
     return f"**{mask_account[-4:]}"
+
+if __name__ == '__main__':
+    card_number = input("введите номер карты: 7158300734726758: ")
+    print(get_mask_card_number(card_number))
+    mask_account = input("введите номер счета: 35383033474447895560 :")
+    print(get_mask_account(mask_account))
--- src/widget.py
+++ src/widget.py
@@ -1,8 +1,5 @@
 from src.masks import get_mask_account, get_mask_card_number
 
-c_b_account = input("введите номер карты или счета: ")
-
-
 def mask_account_card(c_b_account: str) -> str:
     """Функция, которая возвращает строку с замаскированным номером"""
     len_list = ""
@@ -14,15 +11,13 @@ def mask_account_card(c_b_account: str) -> str:
     else:
         return f"{c_b_account[:-20]}{get_mask_account(len_list)}"
 
-
-print(mask_account_card(c_b_account))
-
-data_input = input("Введите дату:")
-
-
 def get_date(data_input: str) -> str:
     """Функция, которая возвращает строку с датой в формате (ДД.ММ.ГГГГ)"""
     return f"{data_input[8:10]}.{data_input[5:7]}.{data_input[:4]}"
 
+if __name__ == '__main__':
+    c_b_account = input("введите номер карты или счета: ")
+    print(mask_account_card(c_b_account))
 
-print(get_date(data_input))
+    data_input = input("Введите дату:")
+    print(get_date(data_input))
