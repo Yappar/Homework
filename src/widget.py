cf28c5be08ccea11c539fd3dce9a76256542d926
linar <linar-88@mail.ru>
2025-04-18 19:07:31 +0500

    final ver2 9.1

--- src/widget.py
+++ src/widget.py
@@ -2,14 +2,15 @@ from src.masks import get_mask_account, get_mask_card_number
 
 def mask_account_card(c_b_account: str) -> str:
     """Функция, которая возвращает строку с замаскированным номером"""
-    len_list = ""
-    for symbol in c_b_account:
-        if symbol.isdigit() == True:
-            len_list += symbol
-    if len(len_list) == 16:
-        return f"{c_b_account[:-16]}{get_mask_card_number(len_list)}"
+    if c_b_account[-17].isdigit() == True:
+    # len_list = ""
+    # for symbol in c_b_account:
+    #     if symbol.isdigit() == True:
+    #         len_list += symbol
+    # if len(len_list) == 16:
+        return f"{c_b_account[:-20]}{get_mask_account(c_b_account)}"
     else:
-        return f"{c_b_account[:-20]}{get_mask_account(len_list)}"
+        return f"{c_b_account[:-16]}{get_mask_card_number(c_b_account)}"
 
 def get_date(data_input: str) -> str:
     """Функция, которая возвращает строку с датой в формате (ДД.ММ.ГГГГ)"""
